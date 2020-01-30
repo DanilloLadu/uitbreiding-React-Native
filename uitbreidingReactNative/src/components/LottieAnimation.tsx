3449890e5e5140273d952fa1243a6a333b801920
Danillo <ladu.danillo@gmail.com>
2020-01-30 21:01:56 +0100

    some lottie stuff

--- uitbreidingReactNative/src/components/LottieAnimation.tsx
+++ uitbreidingReactNative/src/components/LottieAnimation.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import LottieView from "lottie-react-native";
 
 export default class LottieAnimation extends React.Component {
+    private animation: any;
+
+
+    componentDidMount() {
+       this.animation.play();
+        // Or set a specific startFrame and endFrame with:
+       // this.animation.play(10, 11);
+    }
+
     render() {
-        // @ts-ignore
-        return <LottieView source={require('../../assets/lottie.json')} autoPlay loop/>;
+
+        return    <LottieView
+            ref={animation => {
+                this.animation = animation;
+            }}
+            // @ts-ignore
+            source={require('../../assets/lottie.json')}/>;
     }
 
 };
